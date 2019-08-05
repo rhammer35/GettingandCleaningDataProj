b7d8e6fbc33107c017cf7b7c2c7e1f1235041a32
rhammer35 <rhammer35@icloud.com>
2019-08-04 22:35:19 -0400

    Added code to add columns for the variables "subject id" and "activity number" to each data and merge the test and train datasets into one data frame

--- run_analysis.R
+++ run_analysis.R
@@ -17,3 +17,16 @@ train_subject_labels <- read.table("UCI HAR Dataset/train/y_train.txt")
 train_subject_id <- read.table("UCI HAR Dataset/train/subject_train.txt")
 features <- read.table("UCI HAR Dataset/features.txt")
 activities <- read.table("UCI HAR Dataset/activity_labels.txt")
+
+## Create variables in subject data frames for subject id and activity label
+## by merging with other data frames containing that information
+test_subject_data <- mutate(test_subject_data, subjectid = test_subject_id[, 1])
+test_subject_data <- mutate(test_subject_data, activitynum = test_subject_labels[, 1])
+train_subject_data <- mutate(train_subject_data, subjectid = train_subject_id[, 1])
+train_subject_data <- mutate(train_subject_data, activitynum = train_subject_labels[, 1])
+
+## Merge test subject data and train subject data into single data frame
+## This is the first requirement for the project
+merged_data <- bind_rows(test_subject_data, train_subject_data)
+
+
